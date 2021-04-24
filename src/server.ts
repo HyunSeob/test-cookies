ea0ab1f8c0257d8e32d4d4c38915fcb9cf49bf85
HyunSeob <mail@hyunseob.me>
2021-04-24 16:18:55 +0900

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import fastify from 'fastify'
+import fastifyCookie from 'fastify-cookie'
+
+const server = fastify({ logger: true })
+
+server.register(fastifyCookie)
+
+server.get(`/`, async (request, reply) => {
+  return reply
+    .setCookie(`normal`, `yes`)
+    .setCookie(`http-only`, `yes`, { httpOnly: true })
+    .setCookie(`secure`, `yes`, { secure: true })
+    .setCookie(`same-site-strict`, `yes`, { sameSite: `strict` })
+    .setCookie(`same-site-lax`, `yes`, { sameSite: `lax` })
+    .setCookie(`same-site-none`, `yes`, { sameSite: `none` })
+    .send(request.cookies)
+})
+
+const start = async () => {
+  try {
+    await server.listen(3000)
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
